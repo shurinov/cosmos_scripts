c66471cf3d29d5b133b937d250d4a87a252d6ba0
shurinov <@shurinov>
2021-10-25 16:35:21 +0700

    Update readme, minor changes in cos_var_template

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 ## Usage
 1. Copy file **cos_var_template.sh** and rename to **cos_var.sh**
+```
+cd cosmos_scripts
+cp cos_var_template.sh cos_var.sh
+```
 2. Insert your's node parameters in **cos_var.sh**
+```
+nano cos_var.sh    #or another favorite text editor 
+```
+You should change **Common variables** and **Node info** section
+Daemon's ports settings in template are according to cosmos default values. Most likely you will not need to change them.
 3. Change scripts permissions: 
  ```
 chmod +x *.sh 
--- cos_var_template.sh
+++ cos_var_template.sh
@@ -4,6 +4,12 @@
 export COS_BIN_NAME=chain_daemon_binary_name    # for example: umeed, evmosd, etc
 export COS_CHAIN_ID=chain_id_name               # for example: umeevengers-1, evmos_9000-1, etc
 
+# Node info:
+export COS_MONIKER=my_node_name         # change my_node_name to actual node moniker
+export COS_WALLET=my_wallet             # change my_wallet to actual wallet name
+export COS_WALADDR=cosmosXX***X         # change to actual wallet adress in used chain format
+export COS_VALOPER=cosmosvaloperXX***X  # change to actual valoper adress in used chain format
+
 # Ports variables (default values, change if needs):
 # From config/config.toml
 export COS_PORT_PRX=26658
@@ -15,9 +21,3 @@ export COS_PORT_API=1317
 export COS_PORT_ROS=8080
 export COS_PORT_GRPC=9090
 export COS_PORT_GRPCWEB=9091
-
-# Node info:
-export COS_MONIKER=my_node_name         # change my_node_name to actual node moniker
-export COS_WALLET=my_wallet             # change my_wallet to actual wallet name
-export COS_WALADDR=cosmosXX***X         # change to actual wallet adress in used chain format
-export COS_VALOPER=cosmosvaloperXX***X  # change to actual valoper adress in used chain format
