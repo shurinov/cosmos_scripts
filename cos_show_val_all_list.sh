a2374bcaed3052bfe27a4f28b23e3c51776d2f59
shurinov <@shurinov>
2021-10-31 20:10:37 +0700

    Improve validator list script, add show all validators

---
+++ cos_show_val_all_list.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pushd `dirname ${0}` >/dev/null || exit 1
+source ./cos_var.sh
+list_limit=3000 # Magic number for getting list of all validators
+${COS_BIN_NAME} q staking validators -o json --limit=${list_limit} --node "tcp://localhost:${COS_PORT_RPC}" | jq '.validators[]' | jq -r '.tokens + " - " + .description.moniker' | sort -gr | nl 
+popd > /dev/null || exit 1
--- cos_show_val_list.sh
+++ cos_show_val_list.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pushd `dirname ${0}` >/dev/null || exit 1
 source ./cos_var.sh
-${COS_BIN_NAME} q staking validators -o json --limit=1000 --node "tcp://localhost:${COS_PORT_RPC}" | jq '.validators[] | select(.status=="BOND_STATUS_BONDED")' | jq -r '.tokens + " - " + .description.moniker' | sort -gr | nl 
+list_limit=3000 # Magic number for getting list of all validators
+${COS_BIN_NAME} q staking validators -o json --limit=${list_limit} --node "tcp://localhost:${COS_PORT_RPC}" | jq '.validators[] | select(.status=="BOND_STATUS_BONDED")' | jq -r '.tokens + " - " + .description.moniker' | sort -gr | nl 
 popd > /dev/null || exit 1
