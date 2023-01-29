46b42f47e46262af920e7ce682825f87b3978eae
shurinov <@shurinov>
2023-01-29 08:43:47 +0000

    used jq for out formatting

--- cos_show_full_stat.sh
+++ cos_show_full_stat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pushd `dirname ${0}` >/dev/null || exit 1
 source ./cos_var.sh
-curl -s localhost:${COS_PORT_RPC}/status
+curl -s localhost:${COS_PORT_RPC}/status | jq .
 popd > /dev/null || exit 1
