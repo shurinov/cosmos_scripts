cfd3f74fae6e39329f8e32abbcdd238f24e8d812
shurinov <@shurinov>
2023-01-29 08:41:26 +0000

    add --home to cmd

--- cos_show_miss_blocks.sh
+++ cos_show_miss_blocks.sh
@@ -2,5 +2,5 @@
 pushd `dirname ${0}` >/dev/null || exit 1
 source ./cos_var.sh
 FULL_URL="tcp://${COS_NODE_URL}:${COS_PORT_RPC}"
-echo "Missed blocks: `jq -r '.missed_blocks_counter' <<<$($COS_BIN_NAME q slashing signing-info $($COS_BIN_NAME tendermint show-validator) --node=${FULL_URL}  -o json)`"
+echo "Missed blocks: `jq -r '.missed_blocks_counter' <<<$($COS_BIN_NAME q slashing signing-info $($COS_BIN_NAME tendermint show-validator --home ${COS_HOME_PATH}) --node=${FULL_URL}  -o json)`"
 popd > /dev/null || exit 1
